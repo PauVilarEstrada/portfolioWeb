5ef2351c98b9a6bd695baa4ec3ef82f01076f352
Pau Vilar I Estrada <pauvilarbcn@gmail.com>
2025-04-20 17:55:10 +0200

    update hero for navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,6 +6,10 @@ export default function Hero() {
     <section className="hero">
       <h1>Pau Vilar Estrada</h1>
       <h2>Web Developer & Game Creator</h2>
+      <p className="hero-instruction">
+  Hover or tap the <span className="highlight">black circle</span> at the top to navigate through my work.
+</p>
     </section>
+    
   );
 }
