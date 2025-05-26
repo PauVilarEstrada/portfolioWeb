95d75ecf423e057c3556178646dd940825f2d2c7
Pau Vilar I Estrada <pauvilarbcn@gmail.com>
2025-05-26 18:14:12 +0200

    actualizado 26/05/25

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <section className="hero">
       <h1>Pau Vilar Estrada</h1>
-      <h2>Web Developer & Game Creator</h2>
+      <h2>JUNIOR FULL STACK</h2>
       <p className="hero-instruction">
   Hover or tap the <span className="highlight">black circle</span> at the top to navigate through my work.
 </p>
