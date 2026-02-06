f0bc8c10296fec46d2aa33b360ad916229e14af2
Pau Vilar I Estrada <pauvilarbcn@gmail.com>
2026-02-06 17:13:35 +0100

    update specalist

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,8 @@ import "../css/Hero.css";
 export default function Hero() {
   return (
     <section className="hero">
-      <h1>Pau Vilar Estrada</h1>
-      <h2>JUNIOR FULL STACK</h2>
+      <h1>Pau Vilar I Estrada</h1>
+      <h2>JUNIOR FULL STACK DEVELOPER | AI DEVELOPER | DATA ANALIST</h2>
       <p className="hero-instruction">
   Hover or tap the <span className="highlight">black circle</span> at the top to navigate through my work.
 </p>
