a5651e2b9ab7cea01ff8ea31b42212a1b72d2b82
Pau Vilar I Estrada <pauvilarbcn@gmail.com>
2026-02-11 18:39:40 +0100

    new cv & changes hero specalist

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <section className="hero">
       <h1>Pau Vilar I Estrada</h1>
-      <h2>JUNIOR FULL STACK DEVELOPER | AI DEVELOPER | DATA ANALIST</h2>
+      <h2>JUNIOR FULL STACK DEVELOPER | AI & BIG DATA DEVELOPER</h2>
       <p className="hero-instruction">
   Hover or tap the <span className="highlight">black circle</span> at the top to navigate through my work.
 </p>
